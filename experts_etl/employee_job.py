e3516ea707ce97fe2345d03c84a1a4df29b4064d
David Naughton <naughton@umn.edu>
2018-01-17 18:33:04 -0600

    Adds employee_job.group_by_position_nbr().

--- experts_etl/employee_job.py
+++ experts_etl/employee_job.py
@@ -13,6 +13,37 @@ def extract(emplid):
 
   return jobs
 
-def transform(jobs, primary_empl_rcdno):
+"""
+status_flag values:
+C Current 
+F Future 
+H Historical
+
+empl_status values:
+A Active 
+D Deceased 
+L Leave of Absence 
+P Leave With Pay 
+Q Retired With Pay 
+R Retired 
+S Suspended 
+T Terminated 
+U Terminated With Pay 
+V Terminated Pension Pay Out 
+W Short Work Break 
+X Retired-Pension Administration
+"""
+def transform(jobs):
+  jobs_by_position_nbr = group_by_position_nbr(jobs)
+
   t_jobs = []
   return t_jobs
+
+def group_by_position_nbr(jobs):
+  jobs_by_position_nbr = {}
+  for job in jobs:
+    position_nbr = job['position_nbr']
+    if position_nbr not in jobs_by_position_nbr:
+      jobs_by_position_nbr[position_nbr] = [] 
+    jobs_by_position_nbr[position_nbr].append(job)
+  return jobs_by_position_nbr
--- tests/test_employee_job.py
+++ tests/test_employee_job.py
@@ -15,3 +15,32 @@ def test_extract():
     assert re.match(r'^\d+$', job['position_nbr']) 
     assert isinstance(job['effdt'], datetime.date)
     assert isinstance(job['effseq'], int)
+
+def test_group_jobs_by_position_nbr():
+  jobs = [
+    {'position_nbr': '1', 'a': 2, 'b': 3},
+    {'position_nbr': '1', 'a': 3, 'b': 4},
+    {'position_nbr': '2', 'a': 5, 'b': 6},
+    {'position_nbr': '3', 'a': 7, 'b': 8},
+    {'position_nbr': '2', 'a': 9, 'b': 10},
+    {'position_nbr': '1', 'a': 11, 'b': 12},
+  ]
+
+  expected_jobs_by_position_nbr = {
+    '1': [
+      {'position_nbr': '1', 'a': 2, 'b': 3},
+      {'position_nbr': '1', 'a': 3, 'b': 4},
+      {'position_nbr': '1', 'a': 11, 'b': 12},
+    ],
+    '2': [
+      {'position_nbr': '2', 'a': 5, 'b': 6},
+      {'position_nbr': '2', 'a': 9, 'b': 10},
+    ],
+    '3': [
+      {'position_nbr': '3', 'a': 7, 'b': 8},
+    ],
+  }
+
+  jobs_by_position_nbr = employee_job.group_by_position_nbr(jobs)
+  assert jobs_by_position_nbr == expected_jobs_by_position_nbr
+
