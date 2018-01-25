f6100771ac08ea2ec636a6a0a3a1f0fd83c51149
David Naughton <naughton@umn.edu>
2018-01-25 15:39:00 -0600

    Handles cases where a person has no internet id.

--- experts_etl/person.py
+++ experts_etl/person.py
@@ -39,7 +39,10 @@ def extract(emplid):
   return person_dict
 
 def transform(person_dict):
-  person_dict['contact_info_url'] = 'https://myaccount.umn.edu/lookup?type=Internet+ID&CN=' + person_dict['internet_id']
+  if person_dict['internet_id']:
+    person_dict['contact_info_url'] = 'https://myaccount.umn.edu/lookup?type=Internet+ID&CN=' + person_dict['internet_id']
+  else:
+    person_dict['contact_info_url'] = None
 
   person_dict['person_id'] = transform_person_id(
     person_dict['emplid'],
