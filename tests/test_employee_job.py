86fef02f97a9a948ca71a4166a30e112c6164ad3
David Naughton <naughton@umn.edu>
2018-02-15 17:46:59 -0600

    Uses fixtures to improve test_employee_job.test_group_jobs_by_position_nbr().

--- tests/test_employee_job.py
+++ tests/test_employee_job.py
@@ -1,6 +1,7 @@
 from dotenv import load_dotenv, find_dotenv
 load_dotenv(find_dotenv())
 import datetime, re
+import pytest
 from experts_etl import employee_job
 
 def test_extract():
@@ -16,33 +17,13 @@ def test_extract():
     assert isinstance(job['effdt'], datetime.date)
     assert isinstance(job['effseq'], int)
 
-def test_group_jobs_by_position_nbr():
-  jobs = [
-    {'position_nbr': '1', 'a': 2, 'b': 3},
-    {'position_nbr': '1', 'a': 3, 'b': 4},
-    {'position_nbr': '2', 'a': 5, 'b': 6},
-    {'position_nbr': '3', 'a': 7, 'b': 8},
-    {'position_nbr': '2', 'a': 9, 'b': 10},
-    {'position_nbr': '1', 'a': 11, 'b': 12},
-  ]
+@pytest.fixture
+def fake123():
+  from . import fake123_employee_jobs
+  return fake123_employee_jobs
 
-  expected_jobs_by_position_nbr = {
-    '1': [
-      {'position_nbr': '1', 'a': 2, 'b': 3},
-      {'position_nbr': '1', 'a': 3, 'b': 4},
-      {'position_nbr': '1', 'a': 11, 'b': 12},
-    ],
-    '2': [
-      {'position_nbr': '2', 'a': 5, 'b': 6},
-      {'position_nbr': '2', 'a': 9, 'b': 10},
-    ],
-    '3': [
-      {'position_nbr': '3', 'a': 7, 'b': 8},
-    ],
-  }
-
-  jobs_by_position_nbr = employee_job.group_by_position_nbr(jobs)
-  assert jobs_by_position_nbr == expected_jobs_by_position_nbr
+def test_group_jobs_by_position_nbr(fake123):
+  assert employee_job.group_by_position_nbr(fake123.entries) == fake123.entries_by_position_nbr
 
 def test_transform_job_entries():
   entries = [
