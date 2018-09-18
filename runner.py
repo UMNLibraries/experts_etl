1d956e3cce5dc38802d07edf5e32150083ec82a6
David Naughton <naughton@umn.edu>
2018-09-18 12:50:20 -0500

    Improves runner daemon implementation and exception-handling.

--- runner.py
+++ runner.py
@@ -2,8 +2,12 @@ from dotenv import load_dotenv, find_dotenv
 load_dotenv(find_dotenv())
 import importlib
 import multiprocessing as mp
+import os
+import time
 from experts_etl import loggers
 experts_etl_logger = loggers.experts_etl_logger()
+
+default_interval = 14400 # 4 hours, in seconds
 
 extractor_loaders = list(map(
   lambda x: 'experts_etl.extractor_loaders.' + x, 
@@ -30,22 +34,74 @@ transformer_loaders = list(map(
 
 def subprocess(module_name):
   module = importlib.import_module(module_name)
-  module.run(
-    experts_etl_logger=experts_etl_logger
+
+  experts_etl_logger.info(
+    'starting: ' + module_name,
+    extra={
+      'pid': str(os.getpid()),
+      'ppid': str(os.getppid()),
+    }
+  )
+
+  try:
+    module.run(
+      experts_etl_logger=experts_etl_logger
+    )
+  except Exception as e:
+    experts_etl_logger.error(
+      'attempt to execute ' + module_name + '.run() failed: {}'.format(e),
+      extra={
+        'pid': str(os.getpid()),
+        'ppid': str(os.getppid()),
+      }
+    )
+
+  experts_etl_logger.info(
+    'ending: ' + module_name,
+    extra={
+      'pid': str(os.getpid()),
+      'ppid': str(os.getppid()),
+    }
   )
 
 def run():
-  experts_etl_logger.info('starting: experts etl')
+  experts_etl_logger.info(
+    'starting: experts etl',
+    extra={
+      'pid': str(os.getpid()),
+      'ppid': str(os.getppid()),
+    }
+  )
 
   for module_name in extractor_loaders + transformer_loaders:
-    # Must include a comma after a single arg, or multiprocessing seems to
-    # get confused and think we're passing multiple arguments:
-    p = mp.Process(target=subprocess, args=(module_name,))
-    p.start()
-    p.join()
+    try:
+      # Must include a comma after a single arg, or multiprocessing seems to
+      # get confused and think we're passing multiple arguments:
+      p = mp.Process(target=subprocess, args=(module_name,))
+      p.start()
+      p.join()
+    except Exception as e:
+      experts_etl_logger.error(
+        'attempt to execute ' + module_name + 'in a new process failed: {}'.format(e),
+        extra={
+          'pid': str(os.getpid()),
+          'ppid': str(os.getppid()),
+        }
+      )
 
-  experts_etl_logger.info('ending: experts etl')
+  experts_etl_logger.info(
+    'ending: experts etl',
+    extra={
+      'pid': str(os.getpid()),
+      'ppid': str(os.getppid()),
+    }
+  )
   loggers.rollover(experts_etl_logger)
 
+def daemon(interval=default_interval):
+  while True:
+    run()
+    time.sleep(interval)
+
 if __name__ == '__main__':
-  run()
+  daemon()
