0a2a5fc0c8e05b16b61602d7efca3d0f7e2811cd
David Naughton <nihiliad@gmail.com>
2020-11-24 14:39:59 -0600

    Wraps pure_json ops in try/except blocks.

--- experts_etl/pure_to_edw/changes.py
+++ experts_etl/pure_to_edw/changes.py
@@ -63,55 +63,64 @@ def run(
 ):
     if experts_etl_logger is None:
         experts_etl_logger = loggers.experts_etl_logger()
-    experts_etl_logger.info('starting: extracting/loading', extra={'pure_api_record_type': pure_api_record_type})
+    experts_etl_logger.info('starting: extracting/loading raw json', extra={'pure_api_record_type': pure_api_record_type})
 
     client_config = client.Config() if api_version is None else client.Config(version=api_version)
     api_version = client_config.version
 
     with db.cx_oracle_connection() as session:
-        cursor = session.cursor()
-        family_system_names = pure_json.collection_family_system_names_for_api_version(
-            cursor,
-            api_version=api_version
-        )
+        try:
+            cursor = session.cursor()
+            family_system_names = pure_json.collection_family_system_names_for_api_version(
+                cursor,
+                api_version=api_version
+            )
 
-        if startdate_str is None:
-            startdate_str = ensure_valid_startdate(session, api_version, startdate).isoformat()
+            if startdate_str is None:
+                startdate_str = ensure_valid_startdate(session, api_version, startdate).isoformat()
 
-        documents_to_insert = {}
-        for api_document in client.get_all_changes_transformed(startdate_str, config=client_config):
-            if not required_fields_exist(api_document):
-                continue
-            if api_document.familySystemName not in family_system_names:
-                continue
-            if same_or_newer_db_change_exists(cursor, api_document, api_version):
-                continue
+            documents_to_insert = {}
+            for api_document in client.get_all_changes_transformed(startdate_str, config=client_config):
+                if not required_fields_exist(api_document):
+                    continue
+                if api_document.familySystemName not in family_system_names:
+                    continue
+                if same_or_newer_db_change_exists(cursor, api_document, api_version):
+                    continue
 
-            # We make this a dict with unique keys to avoid attempts to insert duplicates:
-            documents_to_insert[f'{api_document.uuid}:{api_document.version}'] = {
-                'uuid': api_document.uuid,
-                'pure_version': api_document.version,
-                'change_type': api_document.changeType,
-                'family_system_name': api_document.familySystemName,
-                'inserted': datetime.now(),
-                'json_document': json.dumps(api_document),
-            }
+                # We make this a dict with unique keys to avoid attempts to insert duplicates:
+                documents_to_insert[f'{api_document.uuid}:{api_document.version}'] = {
+                    'uuid': api_document.uuid,
+                    'pure_version': api_document.version,
+                    'change_type': api_document.changeType,
+                    'family_system_name': api_document.familySystemName,
+                    'inserted': datetime.now(),
+                    'json_document': json.dumps(api_document),
+                }
 
-            if len(documents_to_insert) % transaction_record_limit == 0:
+                if len(documents_to_insert) % transaction_record_limit == 0:
+                    pure_json.insert_change_documents(
+                        cursor,
+                        documents=list(documents_to_insert.values()),
+                        api_version=api_version
+                    )
+                    documents_to_insert = {}
+                    session.commit()
+
+            if documents_to_insert:
                 pure_json.insert_change_documents(
                     cursor,
                     documents=list(documents_to_insert.values()),
                     api_version=api_version
                 )
-                documents_to_insert = {}
-                session.commit()
+            session.commit()
 
-        if documents_to_insert:
-            pure_json.insert_change_documents(
-                cursor,
-                documents=list(documents_to_insert.values()),
-                api_version=api_version
+        except Exception as e:
+            session.rollback()
+            formatted_exception = loggers.format_exception(e)
+            experts_etl_logger.error(
+                f'exception encountered during extractin/loading raw json: {formatted_exception}',
+                extra={'pure_api_record_type': pure_api_record_type}
             )
-        session.commit()
 
-    experts_etl_logger.info('ending: extracting/loading', extra={'pure_api_record_type': pure_api_record_type})
+    experts_etl_logger.info('ending: extracting/loading raw json', extra={'pure_api_record_type': pure_api_record_type})
--- experts_etl/pure_to_edw/collection.py
+++ experts_etl/pure_to_edw/collection.py
@@ -22,49 +22,61 @@ def run(
 ):
     if experts_etl_logger is None:
         experts_etl_logger = loggers.experts_etl_logger()
-    experts_etl_logger.info('starting: extracting/loading', extra={'pure_api_record_type': collection_api_name})
+    experts_etl_logger.info('starting: extracting/loading raw json', extra={'pure_api_record_type': collection_api_name})
 
     client_config = client.Config() if api_version is None else client.Config(version=api_version)
     api_version = client_config.version
 
     with db.cx_oracle_connection() as session:
-        cursor = session.cursor()
+        try:
+            cursor = session.cursor()
 
-        pure_json.process_changes_matching_previous_uuids(
-            cursor,
-            collection_api_name=collection_api_name,
-            api_version=api_version
-        )
-
-        uuids = pure_json.distinct_change_uuids_for_collection(
-            cursor,
-            collection_api_name=collection_api_name,
-            api_version=api_version
-        )
-        uuids_in_pure = []
-        documents_to_insert = {}
-        for api_document in client.filter_all_by_uuid_transformed(collection_api_name, uuids=uuids, config=client_config):
-            uuids_in_pure.append(api_document.uuid)
-
-            if pure_json.document_exists(
+            pure_json.process_changes_matching_previous_uuids(
                 cursor,
-                uuid=api_document.uuid,
                 collection_api_name=collection_api_name,
-                api_version=api_version,
-                staging=True
-            ):
-                continue
+                api_version=api_version
+            )
 
-            documents_to_insert[f'{api_document.uuid}:{api_document.version}'] = {
-                'uuid': api_document.uuid,
-                'pure_created': datetime.strptime(api_document.info.createdDate, iso_8601_format),
-                'pure_modified': datetime.strptime(api_document.info.modifiedDate, iso_8601_format),
-                'inserted': datetime.now(),
-                'updated': datetime.now(),
-                'json_document': json.dumps(api_document),
-            }
+            uuids = pure_json.distinct_change_uuids_for_collection(
+                cursor,
+                collection_api_name=collection_api_name,
+                api_version=api_version
+            )
+            uuids_in_pure = []
+            documents_to_insert = {}
+            for api_document in client.filter_all_by_uuid_transformed(collection_api_name, uuids=uuids, config=client_config):
+                uuids_in_pure.append(api_document.uuid)
 
-            if len(documents_to_insert) % 100 == 0:
+                if pure_json.document_exists(
+                    cursor,
+                    uuid=api_document.uuid,
+                    collection_api_name=collection_api_name,
+                    api_version=api_version,
+                    staging=True
+                ):
+                    continue
+
+                documents_to_insert[f'{api_document.uuid}:{api_document.version}'] = {
+                    'uuid': api_document.uuid,
+                    'pure_created': datetime.strptime(api_document.info.createdDate, iso_8601_format),
+                    'pure_modified': datetime.strptime(api_document.info.modifiedDate, iso_8601_format),
+                    'inserted': datetime.now(),
+                    'updated': datetime.now(),
+                    'json_document': json.dumps(api_document),
+                }
+
+                if len(documents_to_insert) % 100 == 0:
+                    pure_json.insert_documents(
+                        cursor,
+                        documents=list(documents_to_insert.values()),
+                        collection_api_name=collection_api_name,
+                        staging=True,
+                        api_version=api_version
+                    )
+                    session.commit()
+                    documents_to_insert = {}
+
+            if documents_to_insert:
                 pure_json.insert_documents(
                     cursor,
                     documents=list(documents_to_insert.values()),
@@ -72,44 +84,41 @@ def run(
                     staging=True,
                     api_version=api_version
                 )
-                session.commit()
-                documents_to_insert = {}
+            session.commit()
 
-        if documents_to_insert:
-            pure_json.insert_documents(
+            pure_json.process_changes_matching_staging(
                 cursor,
-                documents=list(documents_to_insert.values()),
-                collection_api_name=collection_api_name,
-                staging=True,
-                api_version=api_version
-            )
-        session.commit()
-
-        pure_json.process_changes_matching_staging(
-            cursor,
-            collection_api_name=collection_api_name,
-            api_version=api_version
-        )
-
-        missing_uuids = list((Counter(uuids) - Counter(uuids_in_pure)).elements())
-        for missing_uuids_sublist in [missing_uuids[i:i+100] for i in range(0, len(missing_uuids), 100)]:
-            pure_json.delete_documents_and_changes_matching_uuids(
-                cursor,
-                uuids=missing_uuids_sublist,
                 collection_api_name=collection_api_name,
                 api_version=api_version
             )
 
-        pure_json.load_documents_from_staging(
-            cursor,
-            collection_api_name=collection_api_name,
-            api_version=api_version
-        )
+            missing_uuids = list((Counter(uuids) - Counter(uuids_in_pure)).elements())
+            for missing_uuids_sublist in [missing_uuids[i:i+100] for i in range(0, len(missing_uuids), 100)]:
+                pure_json.delete_documents_and_changes_matching_uuids(
+                    cursor,
+                    uuids=missing_uuids_sublist,
+                    collection_api_name=collection_api_name,
+                    api_version=api_version
+                )
 
-        pure_json.process_changes_matching_previous_uuids(
-            cursor,
-            collection_api_name=collection_api_name,
-            api_version=api_version
-        )
+            pure_json.load_documents_from_staging(
+                cursor,
+                collection_api_name=collection_api_name,
+                api_version=api_version
+            )
 
-    experts_etl_logger.info('ending: extracting/loading', extra={'pure_api_record_type': collection_api_name})
+            pure_json.process_changes_matching_previous_uuids(
+                cursor,
+                collection_api_name=collection_api_name,
+                api_version=api_version
+            )
+
+        except Exception as e:
+            session.rollback()
+            formatted_exception = loggers.format_exception(e)
+            experts_etl_logger.error(
+                f'exception encountered during extractin/loading raw json: {formatted_exception}',
+                extra={'pure_api_record_type': collection_api_name}
+            )
+
+    experts_etl_logger.info('ending: extracting/loading raw json', extra={'pure_api_record_type': collection_api_name})
